00c6fb7003a20e9e57334c2cac510030c56714e2
uruguaj <leo.potocky@protonmail.com>
2024-02-29 21:23:46 +0100

    fix

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
